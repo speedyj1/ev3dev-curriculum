04cccc16c4d39cb5c8b03117d71590089ce68365
Jack Marshall <marshajw@rose-hulman.edu>
2018-01-31 11:03:00 -0500

    m1+m2

--- sandbox/marshajw/motors/m1_drive_timed.py
+++ sandbox/marshajw/motors/m1_drive_timed.py
@@ -77,8 +77,8 @@ Authors: David Fisher and Jack Marshall.
 #      time.sleep(some_amount)
 #      stop()
 #   You may NOT use the advanced motor commands at this time like: run_to_abs_pos, run_to_rel_pos, or run_timed.
-# TODO: 6. Modify the program so that it will exit immediately if the answer to   any   question is 0.
-# TODO: 7. Formally test your work. When you think you have the problem complete run these tests to be sure:
+# DONE: 6. Modify the program so that it will exit immediately if the answer to   any   question is 0.
+# DONE: 7. Formally test your work. When you think you have the problem complete run these tests to be sure:
 #   200 dps 24 inches (make sure it drives within 6 inches of the target distance)
 #   400 dps 24 inches (make sure it drives within 6 inches of the target distance)
 #   800 dps 24 inches (make sure it drives within 6 inches of the target distance)
@@ -86,7 +86,7 @@ Authors: David Fisher and Jack Marshall.
 #   400 dps 36 inches (make sure it drives within 9 inches of the target distance)
 # Do more tests if you see fit.  Ideally you should be +/- 25% of the target goal.
 #
-# TODO: 8. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
+# DONE: 8. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
 #
 #  Observation you should make, the pattern run_forever-->time.sleep-->stop naturally blocks code execution until done.
 
@@ -97,7 +97,7 @@ import time
 def main():
     print("--------------------------------------------")
     print("  Drive using input")
-    print("--------------------------------------------")
+    print("------------------------------------------d--")
     ev3.Sound.speak("Drive using input").wait()
 
     # Connect two large motors on output ports B and C
