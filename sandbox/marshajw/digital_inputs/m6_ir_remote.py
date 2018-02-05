b521f77df7e5582c2097473805016f094d7d1db3
Jack Marshall <marshajw@rose-hulman.edu>
2018-02-05 10:31:05 -0500

    DONE

--- sandbox/marshajw/digital_inputs/m6_ir_remote.py
+++ sandbox/marshajw/digital_inputs/m6_ir_remote.py
@@ -25,8 +25,8 @@
     -- Pressing the Back button will allow your program to end.  It should stop motors, turn on both green LEDs, and
        then print and say Goodbye.  You will need to implement a new robot method called shutdown to handle this task.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Jack Marshall.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
@@ -34,7 +34,7 @@ import time
 import robot_controller as robo
 
 # Note that todo2 is farther down in the code.  That method needs to be written before you do todo3.
-# TODO: 3. Have someone on your team run this program on the EV3 and make sure everyone understands the code.
+# DONE: 3. Have someone on your team run this program on the EV3 and make sure everyone understands the code.
 # Can you see what the robot does and explain what each line of code is doing? Talk as a group to make sure.
 
 
@@ -43,6 +43,12 @@ class DataContainer(object):
 
     def __init__(self):
         self.running = True
+
+
+left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+assert left_motor.connected
+assert right_motor.connected
 
 
 def main():
@@ -68,12 +74,27 @@ def main():
 
     robot.arm_calibration()  # Start with an arm calibration in this program.
 
+    rc1 = ev3.RemoteControl(channel=1)
+
+    rc1.on_red_up = lambda state: left_forward(state)
+    rc1.on_red_down = lambda state: left_backward(state)
+    rc1.on_blue_up = lambda state: right_forward(state)
+    rc1.on_blue_down = lambda state: right_backward(state)
+
+    rc2 = ev3.RemoteControl(channel=2)
+
+    rc2.on_red_up = lambda state: handle_arm_up_button(state, robot)
+    rc2.on_red_down = lambda state: handle_arm_up_button(state, robot)
+    rc2.on_blue_up = lambda state: handle_calibrate_button(state, robot)
+    rc2.on_blue_down = lambda state: handle_shutdown(state, dc)
+
     while dc.running:
         # TODO: 5. Process the RemoteControl objects.
+        rc1.process()
         btn.process()
         time.sleep(0.01)
 
-    # TODO: 2. Have everyone talk about this problem together then pick one  member to modify libs/robot_controller.py
+    # DONE: 2. Have everyone talk about this problem together then pick one  member to modify libs/robot_controller.py
     # as necessary to implement the method below as per the instructions in the opening doc string. Once the code has
     # been tested and shown to work, then have that person commit their work.  All other team members need to do a
     # VCS --> Update project...
@@ -138,6 +159,35 @@ def handle_shutdown(button_state, dc):
     """
     if button_state:
         dc.running = False
+
+
+def left_forward(button_state):
+    if button_state:
+        left_motor.run_forever(speed_sp=900)
+    else:
+        left_motor.stop(stop_action="brake")
+
+
+def right_forward(button_state):
+    if button_state:
+        right_motor.run_forever(speed_sp=900)
+    else:
+        right_motor.stop(stop_action="brake")
+
+
+def left_backward(button_state):
+    if button_state:
+        left_motor.run_forever(speed_sp=-900)
+    else:
+        left_motor.stop(stop_action="brake")
+
+
+def right_backward(button_state):
+    if button_state:
+        right_motor.run_forever(speed_sp=-900)
+    else:
+        right_motor.stop(stop_action="brake")
+
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
